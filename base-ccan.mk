388c68bf8cd273d2368273152b1b4af15cddca0c
Cody P Schafer <devel@codyps.com>
2014-03-22 00:57:41 -0700

    submake machinery is useful in general

--- base-ccan.mk
+++ base-ccan.mk
@@ -8,20 +8,12 @@ ALL_CPPFLAGS += -Iccan
 ALL_LDFLAGS  += -Lccan -lccan
 endif
 
-ifndef V
-	QUIET_SUBMAKE  = @ echo '  MAKE ' $@;
-endif
 
 export CCAN_CFLAGS
 export CCAN_LDFLAGS
 
-define sub-make
-$1 : FORCE
-	$$(QUIET_SUBMAKE)$$(MAKE) $$(MAKE_ENV) $$(MFLAGS) --no-print-directory -C $$(dir $$@) $$(notdir $$@)
-endef
-
-$(eval $(call sub-make-no-clean,ccan/config.h))
-$(eval $(call sub-make-no-clean,ccan/libccan.a))
+$(eval $(call sub-make,ccan/config.h))
+$(eval $(call sub-make,ccan/libccan.a))
 .PHONY: ccan/clean
 $(eval $(call sub-make-no-clean,ccan/clean))
 
